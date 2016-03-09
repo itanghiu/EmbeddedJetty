ff731671c0cf27db5c59c687c43d12a45d995e83
ihiu <ihiu@docapost.fr>
2016-03-09 16:34:20 +0100

    Initial commit

--- src/main/java/com/foryousoft/embeddedjetty/StartJettyServer.java
+++ src/main/java/com/foryousoft/embeddedjetty/StartJettyServer.java
@@ -27,16 +27,10 @@ public class StartJettyServer {
         webAppContext.setDescriptor("web/WEB-INF/web.xml");
         webAppContext.setParentLoaderPriority(true);
 
-        // This webapp will use jsps and jstl. We need to enable the
-        // AnnotationConfiguration in order to correctly set up the jsp container
         Configuration.ClassList classlist = Configuration.ClassList.setServerDefault( server );
         classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
                 "org.eclipse.jetty.annotations.AnnotationConfiguration" );
 
-        // Set the ContainerIncludeJarPattern so that jetty examines these
-        // container-path jars for tlds, web-fragments etc.
-        // If you omit the jar that contains the jstl .tlds, the jsp engine will
-        // scan for them instead.
         webAppContext.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
                 ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$" );
 
